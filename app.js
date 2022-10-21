6cd010411791c9af091373cae2a72d597c125489
LucySoCute <cutelucysu@gmail.com>
2022-10-22 04:02:20 +0800

    modified wrong html on main.handlebars

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 
-app.get('/',(req,res)=> {
+app.get('/', (req, res) => {
   res.render('index')
 })
 app.get('/about', (req, res) => {
